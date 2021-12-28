93a788c6df393eefe7591c49774b9bb59142c372
areekaras <shibili.areekara@gmail.com>
2021-12-28 16:21:21 +0530

    Stage 2: use extension to seperate modules

--- Singleton/Singleton/Singleton.swift
+++ Singleton/Singleton/Singleton.swift
@@ -8,18 +8,20 @@
 import UIKit
 
 
-struct LoggedInUser {}
-struct FeedItem {}
-
-//'s'ingleton
+//Api Module
 class ApiClient {
     static let shared = ApiClient()
     
-    func login(completion: (LoggedInUser) -> Void) {}
-    func loadFeed(completion: (FeedItem) -> Void) {}
+    func execute(_ : URLRequest, completion: (Data) -> Void) {}
 }
 
-class MockApiClient: ApiClient {}
+
+//Login Module
+struct LoggedInUser {}
+
+extension ApiClient {
+    func login(completion: (LoggedInUser) -> Void) {}
+}
 
 class LoginViewController: UIViewController {
     var api = ApiClient.shared //You need this for testing - property dependency injection
@@ -29,6 +31,14 @@ class LoginViewController: UIViewController {
             //show feed screen
         }
     }
+}
+
+
+//Feed Module
+struct FeedItem {}
+
+extension ApiClient {
+    func loadFeed(completion: (FeedItem) -> Void) {}
 }
 
 class FeedViewController: UIViewController {
